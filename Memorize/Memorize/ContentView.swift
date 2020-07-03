4e40053671bfe61f4bf45380b871a3a36148a918
Lorand Fazakas <lorando.fazakas@gmail.com>
2020-07-03 06:48:50 +0200

    Lecture 1

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Fazakas Loránd on 2020. 07. 02..
+//  Copyright © 2020. Lorand Fazakas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            ForEach(0..<4) {_ in
+                CardView(isFaceUp: true)
+            }
+        }
+        .padding()
+        .foregroundColor(.orange)
+        .font(.largeTitle)
+        
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
