57e2ecabede0d0992a9378632d0a33e134f995c4
Lorand Fazakas <lorando.fazakas@gmail.com>
2020-07-08 06:18:05 +0200

    Lecture 3

---
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -0,0 +1,68 @@
+//
+//  EmojiMemoryGameView.swift
+//  Memorize
+//
+//  Created by Fazakas Loránd on 2020. 07. 02..
+//  Copyright © 2020. Lorand Fazakas. All rights reserved.
+//
+
+import SwiftUI
+
+struct EmojiMemoryGameView: View {
+    @ObservedObject var viewModel: EmojiMemoryGame
+    
+    var body: some View {
+        HStack {
+            ForEach(viewModel.cards) { card in
+                CardView(card: card)
+                    .onTapGesture {
+                        self.viewModel.choose(card: card)
+                    }
+                .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+        .padding()
+        .foregroundColor(.orange)
+
+        
+    }
+}
+
+struct CardView: View {
+    var card: MemoryGame<String>.Card
+    
+    var body: some View {
+        GeometryReader { geometry in
+            self.body(for: geometry.size)
+        }
+    }
+    
+    // We can get rid of the self this way
+    func body(for size: CGSize) -> some View {
+        ZStack {
+            if card.isFaceUp {
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                Text(card.content)
+            } else {
+                RoundedRectangle(cornerRadius: cornerRadius).fill()
+            }
+        }
+        .font(.system(size: fontSize(for: size)))
+    }
+    
+    // MARK: - Drawing Constants
+    
+    let cornerRadius: CGFloat = 10.0
+    let edgeLineWidth: CGFloat = 3
+    
+    func fontSize(for size: CGSize) -> CGFloat {
+        min(size.width, size.height) * 0.75
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiMemoryGameView(viewModel: EmojiMemoryGame())
+    }
+}
