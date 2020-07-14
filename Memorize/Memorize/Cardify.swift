5924a65c75a28a621dbf17b08e663e2edbcb8fed
Lorand Fazakas <lorando.fazakas@gmail.com>
2020-07-14 16:21:51 +0200

    Lecture 6 code added

--- Memorize/Memorize/Cardify.swift
+++ Memorize/Memorize/Cardify.swift
@@ -8,20 +8,38 @@
 
 import SwiftUI
 
-struct Cardify: ViewModifier {
+// AnimatableModifier = Animitable + ViewModifier but you cant use the right side of the equation
+struct Cardify: AnimatableModifier {
+    var rotation: Double
     
-    var isFaceUp: Bool
+    init(isFaceUp: Bool) {
+        rotation = isFaceUp ? 0 : 180
+    }
     
+    var isFaceUp: Bool {
+        rotation < 90
+    }
+    
+    // We do this get and set so we can use rotation in the method body instead of animatableData
+    var animatableData: Double {
+        get { return rotation }
+        set { rotation = newValue }
+    }
+    
+    // isFaceUp if else taken out because the card appeared already matched on the screen thus the animation did not
+    // trigger on the second card. We use opacity to hide them instead of completely taking them out of the view
     func body(content: Content) -> some View {
         ZStack {
-            if isFaceUp {
+            Group {
                 RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
                 RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
                 content
-            } else {
-                RoundedRectangle(cornerRadius: cornerRadius).fill()
             }
+                .opacity(isFaceUp ? 1 : 0)
+            RoundedRectangle(cornerRadius: cornerRadius).fill()
+                .opacity(isFaceUp ? 0 : 1)
         }
+        .rotation3DEffect(Angle.degrees(rotation), axis: (0,1,0))
     }
     
     private let cornerRadius: CGFloat = 10.0
--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -12,17 +12,26 @@ struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
     
     var body: some View {
-        Grid(viewModel.cards) { card in
-            CardView(card: card)
-                .onTapGesture {
-                    self.viewModel.choose(card: card)
+        VStack {
+            Grid(viewModel.cards) { card in
+                CardView(card: card).onTapGesture {
+                    withAnimation(.linear(duration: 0.75)) {
+                         self.viewModel.choose(card: card)
+                    }
+                }
+                .aspectRatio(2/3, contentMode: .fit)
+                .padding(5)
             }
-            .aspectRatio(2/3, contentMode: .fit)
-            .padding(5)
-        }
+                
+            .padding()
+            .foregroundColor(.orange)
             
-        .padding()
-        .foregroundColor(.orange)
+            Button(action: {
+                withAnimation(.easeInOut) {
+                    self.viewModel.resetGame()
+                }
+            }, label: {Text("New Game")})
+        }
         
         
     }
@@ -37,6 +46,15 @@ struct CardView: View {
         }
     }
     
+    @State private var animatedBonusRemaining: Double = 0
+    
+    private func startBonusTimeAnimation() {
+        animatedBonusRemaining = card.bonusRemaining
+        withAnimation(.linear(duration: card.bonusTimeRemaining)) {
+            animatedBonusRemaining = 0
+        }
+    }
+    
     // We can get rid of the self using the body func
     // We're turning it into a ViewBuilder because we dont want to return empty view in the else case, nicer this way
     //
@@ -44,13 +62,26 @@ struct CardView: View {
     private func body(for size: CGSize) -> some View {
         if card.isFaceUp || !card.isMatched {
             ZStack {
-                Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true)
-                    .padding(5)
-                    .opacity(0.4)
+                Group {
+                    if card.isConsumingBonusTime {
+                        Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(-animatedBonusRemaining*360-90), clockwise: true)
+                        .onAppear {
+                            self.startBonusTimeAnimation()
+                        }
+                    } else {
+                        Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(-card.bonusRemaining*360-90), clockwise: true)
+                    }
+                }
+                .padding(5).opacity(0.4)
+                .transition(.identity)
                 Text(card.content)
                     .font(.system(size: fontSize(for: size)))
+                    .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+                    .animation(card.isMatched ? Animation.linear(duration: 1).repeatForever(autoreverses: false) : .default)
             }
             .cardify(isFaceUp: card.isFaceUp)
+            .transition(.scale)
+            
         }
     }
     
