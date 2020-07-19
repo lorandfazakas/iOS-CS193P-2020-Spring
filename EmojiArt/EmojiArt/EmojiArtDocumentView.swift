3861518215b33a7780e104c439d1e397f54af4bf
Lorand Fazakas <lorando.fazakas@gmail.com>
2020-07-19 14:08:42 +0200

    lecture 7 code added

---
+++ EmojiArt/EmojiArt/EmojiArtDocumentView.swift
@@ -0,0 +1,75 @@
+//
+//  EmojiArtDocumentView.swift
+//  EmojiArt
+//
+//  Created by Fazakas Loránd on 2020. 07. 19..
+//  Copyright © 2020. Lorand Fazakas. All rights reserved.
+//
+
+import SwiftUI
+
+struct EmojiArtDocumentView: View {
+    @ObservedObject var document: EmojiArtDocument
+    
+    var body: some View {
+        VStack {
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(EmojiArtDocument.palette.map { String($0) }, id: \.self ) { emoji in
+                        Text(emoji)
+                            .font(.system(size: self.defaultEmojiSize))
+                            .onDrag { NSItemProvider(object: emoji as NSString) }
+                    }
+                }
+            }
+            .padding(.horizontal)
+            GeometryReader { geometry in
+                ZStack {
+                    Color(.white).overlay(
+                        Group {
+                            if self.document.backgroundImage != nil {
+                                Image(uiImage: self.document.backgroundImage!)
+                            }
+                        }
+                    )
+                        .edgesIgnoringSafeArea([.horizontal, .bottom])
+                        .onDrop(of: ["public.image", "public.text"], isTargeted: nil) { providers, location -> Bool in
+                            // Probably bug but the location returns the coordinate system for the whole device, not just the View element is
+                            //called on. There is an extension on GeometryProxy to handle this conversion
+                            // This is in IOS coordinate system (upper left (0,0))
+                            var location = geometry.convert(location, from: .global)
+                            location = CGPoint(x: location.x - geometry.size.width/2, y: location.y - geometry.size.height/2)
+                            return self.drop(providers: providers, at: location)
+                    }
+                    ForEach(self.document.emojis) { emoji in
+                        Text(emoji.text)
+                            .font(self.font(for: emoji))
+                            .position(self.position(for: emoji, in: geometry.size))
+                    }
+                }
+            }
+        }
+    }
+    
+    private func font(for emoji: EmojiArt.Emoji) -> Font {
+        Font.system(size: emoji.fontSize)
+    }
+    
+    private func position(for emoji: EmojiArt.Emoji, in size: CGSize) -> CGPoint {
+        CGPoint(x: emoji.location.x + size.width/2, y: emoji.location.y + size.height/2)
+    }
+    
+    private func drop(providers: [NSItemProvider], at location: CGPoint) -> Bool {
+        var found = providers.loadFirstObject(ofType: URL.self) { url in
+            self.document.setBackgroundURL(url)
+        }
+        if !found {
+            found = providers.loadObjects(ofType: String.self) { string in
+                self.document.addEmoji(string, at: location, size: self.defaultEmojiSize)
+            }
+        }
+        return found
+    }
+    
+    private let defaultEmojiSize: CGFloat = 40
+}
